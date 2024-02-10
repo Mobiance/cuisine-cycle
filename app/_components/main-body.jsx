2da4c5188837d72c5f6e3c02258b26d24a5c446b
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-02-11 03:44:19 +0530

    add gemini integration to the website

--- app/_components/main-body.jsx
+++ app/_components/main-body.jsx
@@ -1,9 +1,10 @@
+import { AiPrompt } from "./aiPrompt"
 import SearchRecipes from "./search-recipe"
 
 export const MainBody = () => {
     return (
         <div className="h-full flex items-center justify-center">
-            <SearchRecipes />
+            <AiPrompt />
         </div>
     )
 }
