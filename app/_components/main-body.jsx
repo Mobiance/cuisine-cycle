a950eedc9f1ab707c76e58a80692d07c7e1a0d3a
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-02-10 15:52:57 +0530

    change the component location

--- app/_components/main-body.jsx
+++ app/_components/main-body.jsx
@@ -1,9 +1,9 @@
-// import { AddIngredient } from "./add-ingredient"
-// import { SearchButton } from "./search-button"
 import SearchRecipes from "./search-recipe"
 
 export const MainBody = () => {
     return (
+        <div className="h-full flex items-center justify-center">
             <SearchRecipes />
+        </div>
     )
 }
