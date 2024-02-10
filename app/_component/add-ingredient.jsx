3f19372580be691de1dadd6c0f3e8fe9a70155d7
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-02-10 10:57:38 +0530

    add textarea and add ingredient component

---
+++ app/_component/add-ingredient.jsx
@@ -0,0 +1,9 @@
+import { Textarea } from "@/components/ui/textarea"
+
+export const AddIngredient = () => {
+    return(
+        <div className="p-10">
+            <Textarea placeholder="Type your Ingredients here..."/>
+        </div>
+    )
+}
