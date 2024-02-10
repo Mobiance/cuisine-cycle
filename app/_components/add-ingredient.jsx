5c9bb004f17f21cd22720d98334c38cdede3ea67
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-02-10 15:51:31 +0530

    delete component that are not in use

--- app/_components/add-ingredient.jsx
+++
@@ -1,9 +0,0 @@
-import { Textarea } from "@/components/ui/textarea"
-
-export const AddIngredient = () => {
-    return(
-        <div className="p-10">
-            <Textarea placeholder="Type your Ingredients here..."/>
-        </div>
-    )
-}
